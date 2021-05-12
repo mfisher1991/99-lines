3b238c3ceadc13e6c5023c442e1f43170b80a789
mfisher1991 <83640691+mfisher1991@users.noreply.github.com>
2021-05-12 14:12:26 -0700

    first commit

---
+++ Friends.js
@@ -0,0 +1,28 @@
+let friends = ['James',
+    'Chad',
+    'Chris',
+    'Shanel',
+    'Acasia'
+];
+for (let i = 0; i < friends.length; i++)
+    for (let j = 99; j > 0; j == 1) {
+        console.log() {
+            else if (j == 2)
+        }
+        console.log() {
+            else
+        }
+        console.log()
+    }
+}
+
+
+
+while (counter > 0) {
+    console.log(counter + ' Lines of code in the file, ' + counter + ' lines of code;' +
+        'James strikes one out, Chad clears it all out, ' + counter + ' lines of code in the file, '
+
+    )
+    counter--;
+
+}
